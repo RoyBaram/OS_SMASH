9481ddfcc57e958694c43b335b846b4b5157abd0
Roy <roybaram98@gmail.com>
2025-04-14 12:38:23 +0300

    Added support for more global variables

--- globals.h
+++ globals.h
@@ -6,3 +6,7 @@ extern JobIDs IDs;
 extern set<Job, JobCompare> jobList;
 extern unordered_map<string, function<void(const Command&)>> internalCmds;
 extern string prevWd;
+extern pid_t smashPID;
+extern pid_t currTaskPid;
+extern volatile sig_atomic_t sentSigint;
+extern volatile sig_atomic_t sentSigtstp;
