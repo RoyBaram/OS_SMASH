686bb71a2a67e328d50308c94ab7889fb5743039
Roy <roybaram98@gmail.com>
2025-04-14 12:37:28 +0300

    Added full capture and handling of ctrl+c and ctrl+z inside the smash

--- signals.cpp
+++ signals.cpp
@@ -1,2 +1,30 @@
 // signals.cpp
 #include "signals.h"
+#include "commands.h"
+#include "globals.h"
+
+void sigtstpHandler(int sig) {
+	if (sig == SIGTSTP) {
+		cout << "smash: caught CTRL+Z" << endl;
+		if (currTaskPid == smashPID) {
+			sentSigtstp = SIGNALED;
+		}
+		else {
+			cout << "smash: process " << currTaskPid << " was stopped" << endl;
+			kill(currTaskPid, SIGTSTP);
+		}
+	}
+}
+
+void sigintHandler(int sig) {
+	if (sig == SIGINT) {
+		cout << "smash: caught CTRL+C" << endl;
+		if (currTaskPid == smashPID) {
+			sentSigint = SIGNALED;
+		}
+		else {
+			cout << "smash: process " << currTaskPid << " was killed" << endl;
+			kill(currTaskPid, SIGINT);
+		}
+	}
+}
--- signals.h
+++ signals.h
@@ -4,9 +4,20 @@
 /*=============================================================================
 * includes, defines, usings
 =============================================================================*/
+#include <iostream>
+#include <signal.h>
 
+#define NOT_SIGNALED 0
+#define SIGNALED 1
+
+using namespace std;
 /*=============================================================================
 * global functions
 =============================================================================*/
+
+void sigtstpHandler(int sig);
+
+void sigintHandler(int sig);
+
 
 #endif //__SIGNALS_H__
